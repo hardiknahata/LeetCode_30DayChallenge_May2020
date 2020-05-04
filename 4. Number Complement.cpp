60f63df8894ecaa5e6fb69c730fe84a960f6278b
hardiknahata <hardiknahata@gmail.com>
2020-05-04 21:40:37 +0530

    Update

--- 4. Number Complement.cpp
+++ 4. Number Complement.cpp
@@ -1,3 +1,15 @@
+/*
+Given a positive integer, output its complement number. The complement strategy is to flip the bits of its 
+binary representation.
+
+Input: 5
+Output: 2
+
+Explanation:
+The binary representation of 5 is 101 (no leading zero bits), 
+and its complement is 010. So you need to output 2.
+*/
+
 class Solution {
 public:
     unsigned int findComplement(unsigned int n) 
