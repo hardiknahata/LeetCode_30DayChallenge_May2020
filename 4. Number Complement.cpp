a52db404366ef91e7e5f4e1bc58d89eecbe9b519
Hardik Nahata <40418027+hardiknahata@users.noreply.github.com>
2020-05-05 16:00:52 +0530

    Added second solution.

--- 4. Number Complement.cpp
+++ 4. Number Complement.cpp
@@ -10,6 +10,7 @@ The binary representation of 5 is 101 (no leading zero bits),
 and its complement is 010. So you need to output 2.
 */
 
+// SOLUTION 1 O(n)
 class Solution {
 public:
     unsigned int findComplement(unsigned int n) 
@@ -54,6 +55,25 @@ public:
     }
 };
 
+
+// SOLUTION 2 O(1)
+class Solution {
+public:
+    int findComplement(int n) 
+    {
+        int t=n;
+        int x=1;
+        
+        while(t!=0)
+        {
+            x = x<<1;
+            t = t>>1;
+        }
+        
+    return n^(x-1);
+    }
+};
+
 // Algorithm/Data Structure: Bit Manipulation  
-// TC: O(n)
+// TC: O(1)
 // SC: O(1)
