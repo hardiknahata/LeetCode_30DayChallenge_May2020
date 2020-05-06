8834d6f1ed4149be7c694a7f22f49e8d8e980d30
Hardik Nahata <40418027+hardiknahata@users.noreply.github.com>
2020-05-06 16:50:27 +0530

    Day 1

--- 1. First Bad Version.cpp
+++ 1. First Bad Version.cpp
@@ -1,4 +1,5 @@
 /*
+
 You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version 
 of your product fails the quality check. Since each version is developed based on the previous version, all the versions 
 after a bad version are also bad.
@@ -18,6 +19,7 @@ call isBadVersion(5) -> true
 call isBadVersion(4) -> true
 
 Then 4 is the first bad version. 
+
 */
 
 class Solution {
