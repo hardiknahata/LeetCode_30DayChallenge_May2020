f7eac06cd77e8c55c6aa235ffdcd7fe582a22dbe
Hardik Nahata <40418027+hardiknahata@users.noreply.github.com>
2020-05-06 16:51:07 +0530

    Day 2

--- 2. Jewels and Stones.cpp
+++ 2. Jewels and Stones.cpp
@@ -1,4 +1,5 @@
 /*
+
 You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  
 Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
 
@@ -17,6 +18,7 @@ Note:
 
 S and J will consist of letters and have length at most 50.
 The characters in J are distinct.
+
 */
 
 class Solution {
