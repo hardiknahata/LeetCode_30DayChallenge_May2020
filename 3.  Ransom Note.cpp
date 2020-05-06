f8da0fb73c7f28308278c694bd8c0229ca191650
Hardik Nahata <40418027+hardiknahata@users.noreply.github.com>
2020-05-06 16:51:30 +0530

    Day 3

--- 3.  Ransom Note.cpp
+++ 3.  Ransom Note.cpp
@@ -1,4 +1,5 @@
 /*
+
 Given an arbitrary ransom note string and another string containing letters from all the magazines, write a function that 
 will return true if the ransom note can be constructed from the magazines ; otherwise, it will return false.
 
@@ -10,6 +11,7 @@ You may assume that both strings contain only lowercase letters.
 canConstruct("a", "b") -> false
 canConstruct("aa", "ab") -> false
 canConstruct("aa", "aab") -> true
+
 */
 
 class Solution {
